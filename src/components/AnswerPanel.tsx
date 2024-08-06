1fa0cc285b559d02be8143e9080d5a81704ecc84
Jennifer Fix <codingjennifer@gmail.com>
2024-08-05 18:37:50 -0600

    initial conversion to node app

--- src/components/AnswerPanel.tsx
+++ src/components/AnswerPanel.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { MouseEventHandler } from "react";
 import { Flipper, Flipped } from "react-flip-toolkit";
 import { answerState } from "./states";
--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import AnswerPanel from "./AnswerPanel";
 import { useRecoilValue } from "recoil";
