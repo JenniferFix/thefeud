e0f6921145b594a89f428f1640dfe7b454956fd3
Jennifer Fix <codingjennifer@gmail.com>
2024-08-06 21:24:58 -0600

    get started with supabase and react query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/demo/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/demo/tutorial/SignUpUserSteps";
 import Header from "@/components/demo/Header";
+import Navbar from "@/components/Navbar";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -23,32 +24,21 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
+          <Navbar />
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
-
-      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
+      <div>
+        <h1 className="text-4xl">Welcome to The Feud</h1>
+        <div className="flex gap-4">
+          <div>
+            If you were invited to a game please enter the game code below.
+          </div>
+          <div>
+            Login or create an account to create surveys and host your own game.
+          </div>
+        </div>
       </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
     </div>
   );
 }
---
+++ src/components/LoginPanel.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPanel = () => {
+  return <div>LoginPanel</div>;
+};
+
+export default LoginPanel;
