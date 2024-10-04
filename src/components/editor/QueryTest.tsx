f5e71175bb6b678f89b1c5e6fdfd55c0989f2edc
Jennifer Fix <codingjennifer@gmail.com>
2024-10-04 13:12:58 -0600

    remove another console.log

--- src/components/editor/QueryTest.tsx
+++ src/components/editor/QueryTest.tsx
@@ -6,7 +6,6 @@ const QueryTest = ({ userid }: { userid: string }) => {
   const { isError, data, error, isLoading } = useGetUsersQuestions();
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error: {error.message}</div>;
-  // console.log(data);
   return (
     <div>
       <div>{JSON.stringify(data)}</div>
