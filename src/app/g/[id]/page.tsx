d923104228613888fa7175371d28f8241caae2e9
Jennifer Fix <codingjennifer@gmail.com>
2024-10-08 21:49:03 -0600

    remove GameLoader since file is removed

--- src/app/g/[id]/page.tsx
+++ src/app/g/[id]/page.tsx
@@ -1,4 +1,3 @@
-import GameLoader from '@/components/show/GameLoader';
 import Game from '@/components/show/Game';
 export default function Home({ params }: { params: { id: string } }) {
   return <Game instanceId={params.id} />;
