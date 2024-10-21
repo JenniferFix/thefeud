2c41e032500abb806623ed38e13cbecf41968dc5
Jennifer Fix <codingjennifer@gmail.com>
2024-10-21 16:28:05 -0600

    omg, it was imported twice, doh

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Providers from '@/components/providers/Providers';
 import { Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import Navbar from '@/components/header/Navbar';
 
 export default function RootLayout() {
@@ -11,7 +9,6 @@ export default function RootLayout() {
       <main>
         <Outlet />
       </main>
-      <TanStackRouterDevtools position="bottom-right" />
     </React.Fragment>
   );
 }
